5b08dd490bdfeab35fa2ff7d946e75b1c200aab6
JHLeeeMe <lejung92@gmail.com>
2024-05-07 15:24:04 +0900

    [Update] docstring & typo
    1. Add docstring (init)
    2. typo

--- systemV/mqueue.h
+++ systemV/mqueue.h
@@ -1,16 +1,28 @@
+/// ============================================================================
+/// mqueue.h
+/// ----------------------------------------------------------------------------
+/// Message Queue (System V IPC)
+///
+/// ----------------------------------------------------------------------------
+/// Code Structure
+/// ----------------------------------------------------------------------------
+///
+/// ----------------------------------------------------------------------------
+/// License: The Unlicense <http://unlicense.org/>
+/// ============================================================================
+
 #ifndef MQUEUE_H
 #define MQUEUE_H
 
 
-#define __IPCPLUSPLUS_SYSV_BEGIN namespace IPCplusplus { \
-                                 namespace sysv {
-#define __IPCPLUSPLUS_SYSV_END   }}
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-/// To do
-/// 1. include
+#define __IPCPLUSPLUS_SYSV_BEGIN    namespace IPCplusplus { \
+                                        namespace sysv {
+#define __IPCPLUSPLUS_SYSV_END      }}
 
 __IPCPLUSPLUS_SYSV_BEGIN
-
 namespace mq
 {
     class MQueue
@@ -22,7 +34,6 @@ namespace mq
         /// To do
     };
 }  // IPCplusplus::sysv::mq
-
 __IPCPLUSPLUS_SYSV_END
 
 using namespace ::IPCplusplus;
