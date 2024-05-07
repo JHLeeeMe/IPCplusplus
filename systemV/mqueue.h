a87c761584a28eb661bbcb9f5873745a0838b58d
JHLeeeMe <lejung92@gmail.com>
2024-05-07 14:48:27 +0900

    [Add] message queue header
    1. Add message queue header file (init)

---
+++ systemV/mqueue.h
@@ -0,0 +1,31 @@
+#ifndef MQUEUE_H
+#define MQUEUE_H
+
+
+#define __IPCPLUSPLUS_SYSV_BEGIN namespace IPCplusplus { \
+                                 namespace sysv {
+#define __IPCPLUSPLUS_SYSV_END   }}
+
+/// To do
+/// 1. include
+
+__IPCPLUSPLUS_SYSV_BEGIN
+
+namespace mq
+{
+    class MQueue
+    {
+    public:
+        /// To do
+        /// 1. rule of zero || rule of five
+    private:
+        /// To do
+    };
+}  // IPCplusplus::sysv::mq
+
+__IPCPLUSPLUS_SYSV_END
+
+using namespace ::IPCplusplus;
+
+
+#endif  // MQUEUE_H
