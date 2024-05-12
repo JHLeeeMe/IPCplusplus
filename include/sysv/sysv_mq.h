5a9a0f612b836782f102c38231d33603c5ce078e
이정환 <lejung92@gmail.com>
2024-05-12 18:23:16 +0900

    [Update] MQueue class (Incompletion)
    1. Update `sysv::mq::MQueue``` (incompletion)
    2. Update test cases

--- include/sysv/sysv_mq.h
+++ include/sysv/sysv_mq.h
@@ -1,5 +1,5 @@
 /// ============================================================================
-/// ./include/sysv/sysv_mq.h
+/// sysv_mq.h
 /// ----------------------------------------------------------------------------
 /// Message Queue (System V IPC)
 ///
@@ -11,20 +11,28 @@
 /// License: The Unlicense <http://unlicense.org/>
 /// ============================================================================
 
+/*
+// errno
+EACCES : path를 구성하는 directory중에서 search 권한(x)이 없어서 접근할 수 없습니다.
+EFAULT : path 변수 자체가 잘못된 주소입니다. 
+ELOOP : 너무 많은 symbolic link로 directory가 loop에 빠졌습니다.
+ENAMETOOLONG : path가 너무 길거나 이름이 너무 깁니다.
+ENOENT : path가 빈 문자열이거나 path를 구성하는 directory중에서 없는 directory가 있습니다.
+ENOMEM : 메모리가 부족합니다.
+ENOTDIR : path를 구성하는 directory중에서 directory가 아닌 것이 있습니다.
+EOVERFLOW : 32bit OS에서 컴파일시에 -D_FILE_OFFSET_BITS=64 옵션없이 컴파일하여 파일크기나 
+    inode번호가 64bit에 맞지 않은 경우
+*/
+
 #ifndef MQUEUE_H
 #define MQUEUE_H
 
-/*
-extern int msgctl(int __msqid, int __cmd, struct msqid_ds *__buf) __THROW;
-extern int msgget(key_t __key, int __msgflg) __THROW;
-extern ssize_t msgrcv(int __msqid, void *__msgp, size_t __msgsz, long int __msgtyp, int __msgflg);
-extern int msgsnd(int __msqid, const void *__msgp, size_t __msgsz, int __msgflg);
-*/
 
 #include <sys/ipc.h>  // ftok()
 #include <sys/msg.h>  // msgctl(), msgget(), ...
 
 #include <iostream>
+#include <stdexcept>  // runtime_error()
 
 #define __IPCPLUSPLUS_BEGIN    namespace IPCplusplus {
 #define __IPCPLUSPLUS_END      }
@@ -34,29 +42,165 @@ extern int msgsnd(int __msqid, const void *__msgp, size_t __msgsz, int __msgflg)
 
 __IPCPLUSPLUS_BEGIN
 __SYSV_BEGIN
+namespace utils
+{
+    inline key_t create_key(const std::string& path, const uint8_t proj_id)
+    {
+        if (proj_id == 0)
+        {
+            return -1;
+        }
+
+        return ::ftok(path.c_str(), proj_id);
+    }
+}  // ::IPCplusplus::sysv::utils
+
 namespace mq
 {
+    enum class ePermission
+    {
+        R_____ = 0400, RW____ = 0600,
+        _W____ = 0200, __RW__ = 0060,
+        __R___ = 0040, ____RW = 0006,
+        ___W__ = 0020, RWRWRW = 0666,
+        ____R_ = 0004, RWR_R_ = 0644,
+        _____W = 0002, RW_W_W = 0622,
+    };
+
+    inline ePermission operator|(ePermission lhs, ePermission rhs)
+    {
+        return static_cast<ePermission>(
+            static_cast<uint16_t>(lhs) | static_cast<uint16_t>(rhs)
+        );
+    }
+
     class MQueue
     {
     public:  // rule of five
-        ~MQueue() = default;
+        MQueue(const key_t key, ePermission perm = ePermission::RWR_R_)
+            : key_{ key }
+        {
+            if (create_queue(perm) < 0)
+            {
+                if (err_ == EEXIST)
+                {
+                    queue_id_ = ::msgget(key_, 0);
+                }
 
-        MQueue(const MQueue&) = default;
-        MQueue& operator=(const MQueue&) = default;
+                // throw or ...
+            }
 
-        MQueue(MQueue&&) = default;
-        MQueue& operator=(MQueue&&) = default;
+            if (::msgctl(queue_id_, IPC_STAT, &queue_info_) < 0)
+            {
+                // throw or ...
+            }
+
+            permission_ = static_cast<ePermission>(queue_info_.msg_perm.mode);
+        }
+
+        ~MQueue()
+        {
+            if (remove_queue() < 0)
+            {
+                // throw or ...
+            }
+        }
+
+        MQueue(const MQueue&) = delete;
+        MQueue& operator=(const MQueue&) = delete;
+
+        MQueue(MQueue&&) = delete;
+        MQueue& operator=(MQueue&&) = delete;
+
     public:
-        MQueue() { std::cout << "Hello, world!" << std::endl; }
+        inline ssize_t change_permission(ePermission perm)
+        {
+            queue_info_.msg_perm.mode = static_cast<uint32_t>(perm);
+            if (::msgctl(queue_id_, IPC_SET, &queue_info_) < 0)
+            {
+                err_ = errno;
+                return -1;
+            }
+
+            permission_ = perm;
+
+            return 0;
+        }
+
+        inline ssize_t send(const std::string& msg, int msg_type = 0)
+        {
+            // To do
+        }
+
+        inline ssize_t receive(int msg_type)
+        {
+            // To do
+        }
+
+        inline ePermission get_permission() const
+        {
+            return permission_;
+        }
+
+        inline struct msqid_ds get_queue_info() const
+        {
+            return queue_info_;
+        }
+
+        inline ssize_t get_err() const
+        {
+            return err_;
+        }
+
     private:
-        /// To do
+        inline ssize_t create_queue(ePermission perm)
+        {
+            queue_id_ = ::msgget(key_, IPC_CREAT |
+                                       IPC_EXCL |
+                                       static_cast<uint32_t>(perm));
+            if (queue_id_ < 0)
+            {
+                err_ = errno;
+                return -1;
+            }
+
+            queue_owner_ = true;
+            return 0;
+        }
+
+        inline ssize_t remove_queue()
+        {
+            if (::msgget(key_, 0) < 0)
+            {
+                err_ = errno;
+                return -1;
+            }
+
+            if (::msgctl(queue_id_, IPC_RMID, nullptr) < 0)
+            {
+                err_ = errno;
+                return -1;
+            }
+
+            return 0;
+        }
+
+    private:
+        bool            queue_owner_{};
+
+        key_t           key_{};
+        ePermission     permission_{};
+
+        uint32_t        queue_id_{};
+        struct msqid_ds queue_info_{};
+
+        ssize_t         err_{};
     };
-}  // IPCplusplus::sysv::mq
+}  // ::IPCplusplus::sysv::mq
 __SYSV_END
 __IPCPLUSPLUS_END
 
 namespace sysv = ::IPCplusplus::sysv;
-namespace mq   = ::IPCplusplus::sysv::mq;
 
 
 #endif  // MQUEUE_H
--- test/sysv_mq_test.cpp
+++ test/sysv_mq_test.cpp
@@ -1,21 +1,77 @@
-/// ./test/sysv_mq_test.cpp
+/// sysv_mq_test.cpp
 ///
 
 #include "gtest/gtest.h"
 
 #include "sysv/sysv_mq.h"
 
-TEST(SystemV, sysv_mq)
-{
-    testing::internal::CaptureStdout();
+TEST(sysv_mq, utils)
+{ // IPCplusplus::sysv::utils
+    {  // create_key()
+        ASSERT_EQ(-1, sysv::utils::create_key("./", 0));
+        ASSERT_EQ(-1, sysv::utils::create_key("./", 256));
+        ASSERT_NE(-1, sysv::utils::create_key("./", -1));
 
-    ::IPCplusplus::sysv::mq::MQueue a{};
-                   sysv::mq::MQueue b{};
-                         mq::MQueue c{};
+        ASSERT_EQ(::ftok("./", 255), sysv::utils::create_key("./", 255));
+        ASSERT_EQ(::ftok("./", 1), sysv::utils::create_key("./", 257));
+    }
+}
 
-    std::string output{
-        testing::internal::GetCapturedStdout() };
-    
-    ASSERT_EQ(output,
-              "Hello, world!\nHello, world!\nHello, world!\n");
+TEST(sysv_mq, mq)
+{  // IPCplusplus::sysv::mq
+    {  // ePermission
+        using ePermission_Ty = sysv::mq::ePermission;
+
+        ASSERT_EQ(0666, static_cast<uint32_t>(ePermission_Ty::RWRWRW));
+        ASSERT_EQ(0422, static_cast<uint32_t>(ePermission_Ty::R_____ |
+                                              ePermission_Ty::___W__ |
+                                              ePermission_Ty::_____W));
+        ASSERT_EQ(ePermission_Ty::RWRWRW, ePermission_Ty::RW____ |
+                                          ePermission_Ty::__RW__ |
+                                          ePermission_Ty::____RW);
+    }
+
+    {  // class MQueue
+        using ePermission_Ty = sysv::mq::ePermission;
+        using MQueue_Ty      = sysv::mq::MQueue;
+
+        const key_t    key = sysv::utils::create_key("./", 255);
+        ePermission_Ty flag{ ePermission_Ty::RWR_R_ };
+        MQueue_Ty mqueue{ key, flag };
+
+        {  // change_permision()
+            ASSERT_EQ(0644, static_cast<uint16_t>(mqueue.get_permission()));
+
+            flag = ePermission_Ty::_W____ |
+                   ePermission_Ty::___W__ |
+                   ePermission_Ty::____R_;
+            mqueue.change_permission(flag);
+
+            ASSERT_EQ(0224, static_cast<uint16_t>(mqueue.get_permission()));
+        }
+
+        {  // get_queue_info()
+            struct msqid_ds info{ mqueue.get_queue_info() };
+            std::cout << "info.msg_ctime: "     << info.msg_ctime     << '\n'
+                      << "info.msg_lrpid: "     << info.msg_lrpid     << '\n'
+                      << "info.msg_lspid: "     << info.msg_lspid     << '\n'
+                      << "info.msg_qbytes: "    << info.msg_qbytes    << '\n'
+                      << "info.msg_qnum: "      << info.msg_qnum      << '\n'
+                      << "info.msg_rtime: "     << info.msg_rtime     << '\n'
+                      << "info.msg_stime: "     << info.msg_stime     << '\n'
+                      << "info.msg_perm.uid: "  << info.msg_perm.uid  << '\n'
+                      << "info.msg_perm.cuid: " << info.msg_perm.cuid << '\n'
+                      << "info.msg_perm.gid: "  << info.msg_perm.gid  << '\n'
+                      << "info.msg_perm.cgid: " << info.msg_perm.cgid << '\n'
+                      << "info.msg_perm.mode: " << std::oct << info.msg_perm.mode
+                      << std::endl;
+
+            flag = (ePermission_Ty::RW____ |
+                    ePermission_Ty::__RW__ |
+                    ePermission_Ty::____RW);
+            mqueue.change_permission(flag);
+            ASSERT_EQ(static_cast<uint16_t>(flag),
+                      mqueue.get_queue_info().msg_perm.mode);
+        }
+    }
 }
