e0e4ab2b945257a83732cf924ab997d76e112afc
JHLeeeMe <lejung92@gmail.com>
2024-05-23 17:47:40 +0900

    [Modify] run scripts
    1. Modify container name

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ docker run \
     -e TZ=Asia/Seoul \
     -v common-volume:/common-volume \
     -v $(pwd):/app \
-    --name IPCplusplus-dev \
+    --name IPCplusplus-cpp_dev \
     jhleeeme/cpp:dev \
     bin/bash
